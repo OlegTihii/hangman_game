f450dfec7f18a2fdcc53d12b32759f994fc304c3
OlegTihii <YouDoontKnow>
2024-05-01 20:41:24 +0300

    add second commit

---
+++ src/main/java/org/hangman_game/Start.java
@@ -0,0 +1,11 @@
+package org.hangman_game;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Мы начинаем игру Hang_Man");
+        System.out.println("Если вы хотите прекратить игру, напишите 'Exit'");
+        while (true) {
+
+        }
+    }
+}
